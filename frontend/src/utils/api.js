4293a6be028a3bff4a268c2b815ef605ddc35e39
nikhilhere7 <nikhilmahale456@gmail.com>
2026-07-03 20:16:53 +0530

    screenshot and README update

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -7,7 +7,12 @@ const API = axios.create({
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
-  if (token) {
+  // Don't attach token to public endpoints
+  if (
+    token &&
+    !config.url.includes("/users/login") &&
+    !config.url.includes("/users/register")
+  ) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
